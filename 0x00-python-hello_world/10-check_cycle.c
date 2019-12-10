02ba21bb5d1c40527adfd1f993a1df485db40fbc
darkares23 <desarrollojuseca@gmail.com>
2019-12-09 19:16:15 -0500

    task 10 methos rabit and turtle to see for a loop in a linkened list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check for a loop/cycle in a singly linked list
+ * @list: singly linked list
+ *
+ * Return: 0 if no cyle found, 1 if a cycle is found
+ */
+
 int check_cycle(listint_t *list)
 {
+	listint_t *rabit = list, *turtle = list;
 
+	while (turtle && rabit && rabit->next)
+	{
+		turtle = turtle->next;
+		rabit = rabit->next->next;
+		if (turtle == rabit)
+			return (1);
+	}
+	return (0);
 }
