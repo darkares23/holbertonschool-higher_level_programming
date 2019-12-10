9ea014213119a98afff5219ca2d1048558c738cf
darkares23 <desarrollojuseca@gmail.com>
2019-12-10 12:17:17 -0500

    task 8 converted int to char

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def uppercase(str):
     for char in str:
         if ord(char) >= ord("a") and ord(char) <= ord("z"):
             char = ord(char) - 32
-            char = ord(char)
+            char = chr(char)
         print("{}".format(char), end="")
     print("")
