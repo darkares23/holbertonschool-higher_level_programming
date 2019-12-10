bdda7a991d22fea4c4afce2ca3acb36ee724e47f
darkares23 <desarrollojuseca@gmail.com>
2019-12-09 19:44:30 -0500

    task 10 done fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for a loop/cycle in a singly linked list
+ * check_cycle - check for a cycle in a linked list
  * @list: singly linked list
- *
  * Return: 0 if no cyle found, 1 if a cycle is found
  */
 
