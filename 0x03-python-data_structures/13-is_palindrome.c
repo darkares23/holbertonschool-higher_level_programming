0258f5fb4025c92509f40f42986f9572f8054af6
darkares23 <desarrollojuseca@gmail.com>
2019-12-17 16:21:35 -0500

    task 13: is palindrome a linked list?

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,27 +7,24 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int buff[12];
+	int i = 0, buffer[12], mid_len, j;
 	listint_t *temp = *head;
-	int i, j, k;
 
-	if (temp == NULL)
+	if (*head == NULL || head == NULL)
 		return (1);
-	i = 0;
-	/* populate a list to compare and count number of elements */
+		
 	while (temp)
 	{
-		buff[i] = temp->n;
+		buffer[i] = temp->n;
 		temp = temp->next;
 		i++;
 	}
-	/* find middle of list of numbers */
-	k = (i + 1) / 2;
-	i = i - 1;
-	/* compare front to back of list until middle is reached */
-	for (j = 0; j < k; j++, i--)
+	mid_len = i / 2;
+	i -= 1;
+	
+	for (j = 0; j < mid_len; i--, j++)
 	{
-		if (buff[j] != buff[i])
+		if (buffer[j] != buffer[i])
 			return (0);
 	}
 	return (1);
