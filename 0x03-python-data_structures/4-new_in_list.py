e0537773576a9c771048eb491c2f8cc43f70aed8
darkares23 <desarrollojuseca@gmail.com>
2019-12-17 13:08:11 -0500

    added new len to list in task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or (len(my_list)) < idx:
+    if idx < 0 or (len(my_list) - 1) < idx:
         return (my_list)
     new_list = my_list.copy()
     new_list[idx] = element
