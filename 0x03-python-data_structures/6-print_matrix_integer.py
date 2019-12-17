9ba40fa2a455c48b25a7a7c7ed4fbb1115f0ae08
darkares23 <desarrollojuseca@gmail.com>
2019-12-17 13:27:32 -0500

    task 6: fixed empty matrix case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
             if column < len(matrix[row]) - 1:
