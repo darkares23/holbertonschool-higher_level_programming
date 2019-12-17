614c0687e154b0dc8817fde6cb8a2d8097fea40e
darkares23 <desarrollojuseca@gmail.com>
2019-12-17 15:04:25 -0500

    task 9: finding the higher int of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     num = 0
     if len(my_list) == 0:
-        return (None)
+        return ("None")
     for i in range(len(my_list)):
         if my_list[i] > num:
             num = my_list[i]
