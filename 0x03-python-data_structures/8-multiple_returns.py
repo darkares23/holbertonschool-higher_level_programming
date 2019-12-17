dde507b03b354eda8e4b354548fdb40becfba255
darkares23 <desarrollojuseca@gmail.com>
2019-12-17 14:50:40 -0500

    added case the sentence is empty in task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == 0:
-        _1char = None
-    else:
-        _1char = sentence[0]
-    return (len(sentence), _1char)
+        return (0, None)
+    return (len(sentence), sentence[0])
