23716ac0cc27f992066bd6192317baed045e8ef1
darkares23 <desarrollojuseca@gmail.com>
2020-01-09 18:50:14 -0500

    task 7 definition of node class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,33 +2,36 @@
 
 """ Defines a node in a sinly linked list"""
 
-class Node:
-	def __init__(self, data, next_node=Node):
-		"""Initiates the instance"""
-		self.data = data
-		self.next_node = next_node
-	@property
-	def data(self):
-		return self.data
-	
-	@data.setter
-	def data(self, value):
-		if type(value) != int:
-			raise TypeError("data must be an integer")
-		else:
-			self.__data = value
-	@property
-	def next_node(self):
-		return self.next_node
 
-	@next_node.setter
-	def next_node(self, value):
-		if type(value) != Node or value is not None:
-			raise TypeError("next_node must be a Node object")
-		else:
-			self.__spot = value
-	
+class Node:
+    def __init__(self, data, next_node=Node):
+        """Initiates the instance"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.data
+
+    @data.setter
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) != Node or value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__spot = value
+
+
 class SinglyLinkedList:
-	def __init__(self):
-		self.__head = None
-	
+    def __init__(self):
+        self.__head = None
