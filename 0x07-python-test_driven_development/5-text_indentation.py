a2400de85188273d580a24108a2cbeb2a2a567ff
darkares23 <desarrollojuseca@gmail.com>
2020-01-13 16:35:58 -0500

    task 6 unitest python3

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
-text identation module
+function that prints a text with 2 new lines
+after these characters: ., ? and :
 """
 
 
 def text_indentation(text):
+    """
+    print text funct
+    """
+
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    flag = 0
+    new_text = ""
     delim = {".", "?", ":"}
-
-    for char in text:
-        if char in delim:
-            print(char, end="")
+    i = 0
+    for i in range(len(text)):
+        if text[i] in delim:
+            new_text += text[i]
+            new_text = new_text.strip()
+            print(new_text, end='')
             print("\n")
-            flag = 1
+            new_text = ""
+            i += 1
         else:
-            if flag == 0:
-                print(char, end="")
-            else:
-                if char == " " or char == "\t":
-                    pass
-                else:
-                    print(char, end="")
-                    flag = 0
+            new_text += text[i]
+            i += 1
+    if new_text != "":
+        print(new_text.strip(), end='')
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Class test for 6-max_interger.py """
+
+    def test_max_integer_neg(self):
+        """ test case for normal list of integers w/ negatives
+        """
+        test_list = [1, 2, 3, 8, 4, -40, -400, -12, 0]
+        self.assertEqual(max_integer(test_list), 8)
+
+    def test_max_integer_(self):
+        """ test case for empty list
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_one_element(self):
+        """test max_ordered
+        """
+        self.assertEqual(max_integer([23]), 23)
+
+    def test_max_integer(self):
+        """ test case for normal list of integers w/o negatives
+        """
+        test_list = [1, 2, 3, 8, 4]
+        self.assertEqual(max_integer(test_list), 8)
+
+    def test_max_same_number(self):
+        """test max_ordered
+        """
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+
+    def test_string_list(self):
+        """test max_ordered
+        """
+        with self.assertRaises(TypeError):
+            max_integer('hello', 'there')
+
+    def test_string(self):
+        """test max_ordered
+        """
+        self.assertEqual(max_integer("hello"), 'o')
+
+    def test_return_none(self):
+        """test max_ordered
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_float(self):
+        """test max_ordered
+        """
+        self.assertEqual(max_integer([-3.23, 23.43, 44.2]), 44.2)
+
+if __name__ == '__main__':
+    unittest.main()
