d2f38716dbd1479f574ea955b7579167a89e79b4
darkares23 <desarrollojuseca@gmail.com>
2020-01-14 04:22:42 -0500

    Changed task 0 cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,19 +7,13 @@ Return the sum
 
 
 def add_integer(a, b=98):
-    """This function returns the sum of two int
     """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
-    if a != a:
-        raise TypeError('a must be an integer')
-    if b != b:
-        raise TypeError('a must be an integer')
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-
-    return (a + b)
+    This function returns the sum of two int
+    """
+    if (a is None):
+        raise TypeError("a must be an integer")
+    if (type(a) is not int) and (type(a) is not float):
+        raise TypeError("a must be an integer")
+    if (type(b) is not int) and (type(b) is not float):
+        raise TypeError("b must be an integer")
+    return int(a + b)
