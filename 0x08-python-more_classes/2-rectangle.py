565120c9c5c8545dbaf85f0b630fd8011eeb926b
darkares23 <desarrollojuseca@gmail.com>
2020-01-15 13:03:55 -0500

    deleted new task 1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise TypeError('height must be >= 0')
+        self.__height = value
 
     @property
     def width(self):
@@ -10,32 +22,18 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")@property
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        return self.__width
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise TypeError('width must be >= 0')
+        self.__width = value
 
     def area(self):
-        return self.__width * self.__height
+        return self.__height * self.__width
 
     def perimeter(self):
         if self.width == 0 or self.height == 0:
-            return(0)
+            Perimeter = 0
         else:
-            perimeter = 2 * self.__width + 2 * self.__height
-            return perimeter
+            Perimeter = self.__height * 2 + self.__width * 2
+        return Perimeter
