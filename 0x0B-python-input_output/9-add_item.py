21c1e01213d1cc6ce6e82d69d4162f3def52c351
darkares23 <desarrollojuseca@gmail.com>
2020-01-21 17:24:00 -0500

    changed task 9 code structure

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,9 +4,9 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    try:
-        data = load_from_json_file("add_item.json")
-        save_to_json_file(new_obj, "add_item.json")
-    except:
-        save_to_json_file(sys.argv[1:], "add_item.json")
+
+try:
+    data = load_from_json_file("add_item.json")
+    save_to_json_file(new_obj, "add_item.json")
+except:
+    save_to_json_file(sys.argv[1:], "add_item.json")
