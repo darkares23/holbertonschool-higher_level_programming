82dbd8cfa8f63d9b3b1fb9fcfa8c8000f5d4b6a0
darkares23 <desarrollojuseca@gmail.com>
2020-01-21 17:03:22 -0500

    task 8 and 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    try:
+        data = load_from_json_file("add_item.json")
+    except:
+        new_obj = []
+        open("add_item.json", 'w')
+        save_to_json_file(new_obj, "add_item.json")
+
+    data = load_from_json_file("add_item.json")
+    for i in range(1, len(sys.argv)):
+        data.append(sys.argv[i])
+    save_to_json_file(data, "add_item.json")
