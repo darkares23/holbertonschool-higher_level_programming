99b4703df30a88ee725bbc97ee98cc79c23553b2
darkares23 <desarrollojuseca@gmail.com>
2020-01-21 17:30:35 -0500

    task 9 copy

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,10 +2,8 @@
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-
 try:
-    data = load_from_json_file("add_item.json")
-    save_to_json_file(data + (sys.argv[1:]), "add_item.json")
+    new_list = load_from_json_file("add_item.json")
+    save_to_json_file(new_list + sys.argv[1:], "add_item.json")
 except Exception:
     save_to_json_file(sys.argv[1:], "add_item.json")
