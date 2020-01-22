483ca52c5e4382797081115031820cfa36b2891d
darkares23 <desarrollojuseca@gmail.com>
2020-01-21 19:25:56 -0500

    added task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and hasattr(self, '__dict__'):
+            new_dic = {}
+            for at in attrs:
+                if at in self.__dict__ and type(at) is str:
+                    new_dic[at] = self.__dict__[at]
+            return new_dic
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            self.__dict__[k] = v
