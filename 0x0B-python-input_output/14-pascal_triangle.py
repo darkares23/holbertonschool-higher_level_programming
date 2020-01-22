5f6f4a39648dbbfe62b6cb84ea6bc413bfdba5ba
darkares23 <desarrollojuseca@gmail.com>
2020-01-21 19:34:00 -0500

    added task 14 pep8

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-	if n <= 0:
-		return []
-	pascal = []
-	if n > 1:
-		for i in range(n):
-			pascal.append(rows(i))
-	return pascal
+    if n <= 0:
+        return []
+    pascal = []
+    if n > 1:
+        for i in range(n):
+            pascal.append(rows(i))
+    return pascal
+
 
 def rows(n):
-	row = str(11 ** n)
-	return [num for num in row]
+    row = str(11 ** n)
+    return [num for num in row]
