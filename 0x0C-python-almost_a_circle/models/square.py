69b9cc5c27e6a4df5ff7ebb08be77937cad8a55b
Juan Ocampo <desarrollojuseca@gmail.com>
2020-01-27 13:44:49 -0500

    added commets to sqaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,22 +2,36 @@
 """
 Square class module
 """
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    """
+    Square class
+    """
     def __init__(self, size, x=0, y=0, id=None):
-		"""Constructor"""
+        """
+        Constructor
+        """
         super().__init__(id=id, x=x, y=y, width=size, height=size)
 
     def __str__(self):
+        """
+        Constructor
+        """
+        super()
         string = "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
                                                self.id, self.x,
                                                self.y, self.width)
         return string
 
     def update(self, *args, **kwargs):
+        """
+        Constructor
+        """
+        super()
         if args:
             att_list = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
@@ -27,15 +41,27 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """
+        Constructor
+        """
+        super()
         dic_string = {'id': self.id, 'size': self.width,
                       'x': self.x, 'y': self.y}
         return dic_string
 
     @property
     def size(self):
+        """
+        Constructor
+        """
+        super()
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Constructor
+        """
+        super()
         self.width = value
         self.height = value
