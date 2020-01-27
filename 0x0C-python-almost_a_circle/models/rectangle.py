527092fe460e721d4674283d77ee8b41ad25f38a
Juan Ocampo <desarrollojuseca@gmail.com>
2020-01-27 12:51:28 -0500

    commited class and shebang

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Rectangle class
+"""
+
+
 from models.base import Base
 import json
 
 
 class Rectangle(Base):
-
+	"""Rectangle class """
     __nb_objects = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
