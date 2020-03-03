bdafbddb2d16ad079d4eb24c5f80695d057b3d50
darkares23 <desarrollojuseca@gmail.com>
2020-03-03 16:21:05 -0500

    added task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- mport the database dump from hbtn_0d_tvshows to your MySQL server: download
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id ORDER BY title, genre_id ASC;
