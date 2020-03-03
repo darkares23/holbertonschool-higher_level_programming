8a023efdfe25dd3b13a00ff32cb1683a22c6993c
darkares23 <desarrollojuseca@gmail.com>
2020-03-03 14:02:55 -0500

    task 1 to 3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS user_0d_1@localhots IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL ON *.* TO user_0d_1@localhots;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhots' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhots';
