6981a84f203e7fd2f043b8691f6069a6e56380d4
darkares23 <desarrollojuseca@gmail.com>
2020-03-28 13:14:53 -0500

    fixed sintax error on task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,23 +6,18 @@ ists all states with a name starting with N
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-    connection = MySQLdb.Connect(host="localhost",
-                                 port=3306,
-                                 user=username,
-                                 passwd=password,
-                                 db=dbname,
-                                 charset="utf8")
-    cursor_exe = connection.cursor()
-    cursor_exe.execute('''
-                        SELECT * FROM states WHERE states.name
-                        LIKE 'N%' ORDER BY id ASC
-                        ''')
-    qRows = cursor_exe.fetchall()
-    for rows in qRows:
-        print(rows)
-    cursor_exe.close()
-    connection.close()
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute('''
+                SELECT * FROM states WHERE states.name
+                LIKE 'N%' ORDER BY id ASC
+                ''')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
