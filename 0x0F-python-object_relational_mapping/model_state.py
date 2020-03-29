bd76f5d4f6990b9f3c9983be0dd2abad4f1094cc
darkares23 <desarrollojuseca@gmail.com>
2020-03-29 10:56:13 -0500

    fixed style pep8 for model base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-	"""Class state"""
-	__tablename__ = 'states'
-	id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-	name = Column(String(128), nullable=False)
+    """Class state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
