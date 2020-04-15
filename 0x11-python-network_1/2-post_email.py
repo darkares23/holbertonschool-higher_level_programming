9b9a0fc4c1a677c6b247e5caaf6fce989f2b75da
darkares23 <desarrollojuseca@gmail.com>
2020-04-15 15:55:43 -0500

    added task 4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,12 @@ from urllib.parse import urlencode
 
 
 if __name__ == "__main__":
-    def getEmail():
-        url = argv[1]
-        values = {'email': argv[2]}
+    url = argv[1]
+    values = {'email': argv[2]}
 
-        data = urlencode(values)
-        data = data.encode('ascii')
-        req = Request(url, data)
-        with urlopen(req) as res:
-            content = res.read().decode('utf-8')
-            print(content)
+    data = urlencode(values)
+    data = data.encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as res:
+        content = res.read().decode('utf-8')
+        print(content)
