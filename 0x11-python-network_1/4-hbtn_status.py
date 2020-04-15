38e338216f2188da4ca90e38a4ecea2d4b25e367
darkares23 <desarrollojuseca@gmail.com>
2020-04-15 15:42:47 -0500

    added task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,11 +5,13 @@
 import requests
 
 
-if __name__ == "__main__":
-    def getStatus():
-        req = requests.get('https://intranet.hbtn.io/status')
-        content = req.text
+def getStatus():
+    req = requests.get('https://intranet.hbtn.io/status')
+    content = req.text
 
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+if __name__ == "__main__":
+    getStatus()
