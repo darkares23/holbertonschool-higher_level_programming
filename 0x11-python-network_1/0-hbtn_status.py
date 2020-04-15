91d0cd55ae8ac88c6b790ec49f929d477f2fef22
darkares23 <desarrollojuseca@gmail.com>
2020-04-15 13:12:29 -0500

    added comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+"""Python script that fetches"""
 import urllib.request
 import sys
 
@@ -15,5 +13,6 @@ def getStatus():
         print("\t- type: {}".format(type_content))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
 if __name__ == "__main__":
     getStatus()
