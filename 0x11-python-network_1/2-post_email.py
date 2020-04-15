30fd08fc4d5c4004b1f1f520a1cf259962291501
darkares23 <desarrollojuseca@gmail.com>
2020-04-15 14:11:56 -0500

    added main post and response task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,11 +6,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-	def getEmail():
-		"""Post and print the response"""
-		data = urllib.parse.urlencode({'email': argv[2]})
-		data = data.encode('ascii')
-		req = urllib.request.Request(argv[1], data)
-		with urllib.request.urlopen(req) as res:
-			content = res.read()
-			print(content.decode('utf-8'))
+    def getEmail():
+        """Post and print the response"""
+        data = urllib.parse.urlencode({'email': argv[2]})
+        data = data.encode('ascii')
+        req = urllib.request.Request(argv[1], data)
+        with urllib.request.urlopen(req) as res:
+            content = res.read()
+            print(content.decode('utf-8'))
