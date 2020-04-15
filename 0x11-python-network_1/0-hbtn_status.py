92628d3326a3df8338851528ca8a5731e68d943b
darkares23 <desarrollojuseca@gmail.com>
2020-04-15 13:10:02 -0500

    added task 0 fetches page rrl

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ def getStatus():
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         type_content = type(content)
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type_content))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
