098007cdf2dc5a50e4f4df186d4366699eb07c46
darkares23 <desarrollojuseca@gmail.com>
2020-04-16 09:20:30 -0500

    verified name and id sexist on set

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """takes in a URL and an email address,
 sends a POST request to the passed URL"""
-import requests
 from sys import argv
+import requests
 
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     try:
         _data = {'q': q}
         url = 'http://0.0.0.0:5000/search_user'
-        req = requests.post(url, data=_data).json()
+        req = requests.post(url, _data).json()
         if not req:
             print("No result")
         else:
