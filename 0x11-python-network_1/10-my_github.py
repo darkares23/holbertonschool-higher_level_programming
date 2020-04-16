4927b1f9d98f87d9cfbb9e33774eb259f18a1586
darkares23 <desarrollojuseca@gmail.com>
2020-04-16 09:29:10 -0500

    verified name and id sexist on set

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://api.github.com/user"
     _auth = (argv[1], argv[2])
-    req = requests.get(url, auth=_auth)
+    req = requests.get(url, auth=_auth).json()
     reqId = req.get('id')
     print(reqId)
