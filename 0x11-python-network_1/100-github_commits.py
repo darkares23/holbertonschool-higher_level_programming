b283554b1644972aefbed8647dd6515c04ef81e7
darkares23 <desarrollojuseca@gmail.com>
2020-04-16 20:42:53 -0500

    added first advanced task- print 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/" + argv[2] + "/" + argv[1] + "/commits"
+    _auth = (argv[1], argv[2])
+    req = requests.get(url).json()
+    commits = 0
+    for line in req:
+        print("{}: {}".format(line['sha'], line['commit']['autor']['name']))
+        commits += 1
+        if commits == 10:
+            exit()
