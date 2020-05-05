5a030cb44ccaf2ee1288eeed7e19a8e0fbf614fb
darkares23 <desarrollojuseca@gmail.com>
2020-05-05 13:17:14 -0500

    adde parseInt to conver strig number to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (isNaN(process.argv[2])) {
   console.log('Not A Number');
 } else {
-  console.log('My number: ' + process.argv[2]);
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
