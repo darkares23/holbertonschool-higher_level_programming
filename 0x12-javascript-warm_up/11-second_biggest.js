736d228cf5e1394a0800bcebd86fb6e2cc3e6aaa
darkares23 <desarrollojuseca@gmail.com>
2020-05-05 14:57:16 -0500

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).sort().reverse();
-  console.log(paserInt(args[1]));
+  const args = process.argv.slice(2).sort(function (a, b) { return b - a; });
+  console.log(args[1]);
 }
