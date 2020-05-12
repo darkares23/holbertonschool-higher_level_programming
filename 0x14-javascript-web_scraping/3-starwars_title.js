2c6a4703380fa734dc0b50dc61deedb52dad4039
darkares23 <desarrollojuseca@gmail.com>
2020-05-12 13:09:45 -0500

    task 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
-const args = process.argv;
-const url = args[2];
 const request = require('request');
-request(url, function (error, response) {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('code:', response && response.statusCode);
-});const request = require('request');
 const episode = process.argv[2];
 
 request('http://swapi.co/api/films/' + episode, (err, res, body) => {
